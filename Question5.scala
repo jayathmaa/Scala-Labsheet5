45aa0748c5c1c13ea8d48b3054ac60bf1e183466
jayathmaa <jayathmavidu@gmail.com>
2023-07-11 17:27:42 +0530

    Edited a bit

--- Question5.scala
+++ Question5.scala
@@ -2,7 +2,7 @@ package Tutorial5
 
 import scala.io.StdIn
 
-object Question5 {
+object Question51 {
   def isEven(x:Int):Boolean=if(x%2==0){return true}else{return false}
   def addEven(n:Int):Int=if(n==0){return 0}else if(n==1){return 0}else if(isEven(n)){return (n+addEven(n-2))}else{return addEven(n-1)}
 
