0000cba53cd6550c6f61f93206dfcb8c0d16d5d5
rupriate <rusirasathnindu50@gmail.com>
2023-07-11 06:44:30 +0530

    Question 6

---
+++ Question5.scala
@@ -0,0 +1,17 @@
+package T5
+
+import scala.io.StdIn
+
+object Question5 {
+  def isEven(x:Int):Boolean=if(x%2==0){return true}else{return false}
+  def addEven(n:Int):Int=if(n==0){return 0}else if(n==1){return 0}else if(isEven(n)){return (n+addEven(n-2))}else{return addEven(n-1)}
+
+
+
+  def main(args: Array[String]): Unit= {
+    printf("Enter n : ");
+    val n=scala.io.StdIn.readInt();
+    printf("%d",addEven(n));
+
+  }
+}
